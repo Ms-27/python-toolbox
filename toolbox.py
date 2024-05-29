7bef3f62457b7cfa5c1274c9d81e5f70b8b0ace5
Ms <ms@juliver.com>
2024-05-29 10:55:48 +0200

    feat(toolbox.py): multithread function added

--- toolbox.py
+++ toolbox.py
@@ -1,7 +1,9 @@
 from functools import wraps
 from time import time
+import concurrent.futures
+import tqdm
 
-
+#
 def timer(f):
     '''
         Decorator to return function duration
@@ -15,3 +17,11 @@ def timer(f):
         print (f'{f.__name__} was executed in {duration:.4f} secs')
         return value
     return wrapper
+
+#multithread for a list + tqdm
+def handle_requests(func, issues: list, num_workers: int = 5):
+    '''
+        Function to execute function on several items of an list by exploiitng multithreading 
+    '''
+    with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+        return list(tqdm(executor.map(function, issues), total=len(issues)))
