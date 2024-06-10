eaa583e694f28bd9c58866520bb2b61fe9cc38b2
Ms-27 <ms@juliver.com>
2024-06-10 13:49:01 +0200

    Create list_copy.py

---
+++ list_copy.py
@@ -0,0 +1,10 @@
+import copy
+
+
+# do a shallow copy of a list:
+#  new list but , elements remain the same objetcs
+new_list = list[:]
+
+# do a deepcopy of a list:
+# new list with new elements
+new_list = copy.deepcopy(list)
