9490b9e3ec2819ed581a30d8d907de6e5c22491c
Ms-27 <ms@juliver.com>
2024-06-11 10:32:33 +0200

    feat(toolbox): time_algorithm function added

--- toolbox.py
+++ toolbox.py
@@ -1,7 +1,5 @@
 from functools import wraps
 from time import time
-import concurrent.futures
-import tqdm
 
 #
 def timer(f):
@@ -18,6 +16,11 @@ def timer(f):
         return value
     return wrapper
 
+###===###
+
+import concurrent.futures
+import tqdm
+
 #multithread for a list + tqdm
 def handle_requests(func, issues: list, num_workers: int = 5):
     '''
@@ -25,3 +28,17 @@ def handle_requests(func, issues: list, num_workers: int = 5):
     '''
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
         return list(tqdm(executor.map(function, issues), total=len(issues)))
+
+###===###
+
+from timeit import repeat
+
+def time_algorithm(algorithm, array):
+    '''
+        Function that run an algorithm a certain number of time and print minimum time execution 
+    '''
+    setup_code = f"from __main__ import {algorithm}"
+    stmt = f"{algorithm}({array})"
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
